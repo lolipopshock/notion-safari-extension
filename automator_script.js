0ade46a3e7c64fd94c5c162844b55b26f5ddad0b
lolipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-07-12 15:13:27 -0400

    Add support for different browsers

--- automator_script.js
+++ automator_script.js
@@ -1,7 +1,12 @@
 function run(input, parameters) {
-    var app = Application.currentApplication()
+    
+	var frontAppName = Application("System Events").processes.whose({frontmost: {'=': true }})[0].name();
+	var Browser = Application(frontAppName)
+	Browser.includeStandardAdditions = true
+	
+	var app = Application.currentApplication()
     app.includeStandardAdditions = true
-
+	
     var returnVals = ["--page_selection"];
 
     // Obtain the Notion information to construct the Selection Pannel 
@@ -9,16 +14,17 @@ function run(input, parameters) {
         return app.read(Path(file))
     }
     jsonFile = readFile(Path('<your/notion/config/file/path>'))
-    var notonInfo = JSON.parse(jsonFile)
-    let functionChoices = Object.keys(notonInfo["pages"]).concat(["Custom"])
+    var notionInfo = JSON.parse(jsonFile)
+    let functionChoices = Object.keys(notionInfo["pages"]).concat(["Custom"])
 
     // Fetch user selection 
     var pageSelection = app.chooseFromList(functionChoices, {
         withPrompt: "Which page do you want to add to?",
         defaultItems: functionChoices[0]
     })
-
+	
     // Parse the user selection
+	
     if (pageSelection[0] == "Custom") {
         // Allow user input
         var response = app.displayDialog("Please paste the link to the Notion Page", {
@@ -28,8 +34,10 @@ function run(input, parameters) {
             defaultButton: "Continue"
         })
         returnVals.push(response.textReturned)
+		var pageUrl=response.textReturned;
     } else if (pageSelection[0] != undefined) {
         returnVals.push(pageSelection[0])
+		var pageUrl=notionInfo["pages"][pageSelection[0]];
     } else (
         returnVals.push(false)
     )
@@ -37,13 +45,19 @@ function run(input, parameters) {
     // Fetch the data from the given application
     titleVals = ["--titles"]
     urlVals = ["--urls"]
-    var Safari = Application('Safari');
-    Safari.includeStandardAdditions = true;
+
+	if (frontAppName == "Google Chrome") {
+	    var tab = Browser.windows[0].activeTab
+	} else {
+	    var tab = Browser.windows[0].currentTab() 
+	}
     
-    var tab = Safari.windows[0].currentTab() 
 	urlVals.push(tab.url())
     titleVals.push(tab.name())
-
+    
+    //Open the notion page
+	var notionSchema = pageUrl.replace("https://", "notion://")
+	app.doShellScript(`open "${notionSchema}"`)
     // Return the values 
     returnVals = returnVals.concat(titleVals).concat(urlVals)
     return returnVals
--- automator_script_batch.js
+++ automator_script_batch.js
@@ -1,7 +1,13 @@
 function run(input, parameters) {
-    var app = Application.currentApplication()
+    
+    // Support different types of browsers
+	var frontAppName = Application("System Events").processes.whose({frontmost: {'=': true }})[0].name();
+	var Browser = Application(frontAppName)
+	Browser.includeStandardAdditions = true
+		
+	var app = Application.currentApplication()
     app.includeStandardAdditions = true
-
+	
     var returnVals = ["--page_selection"];
 
     // Obtain the Notion information to construct the Selection Pannel 
@@ -9,15 +15,15 @@ function run(input, parameters) {
         return app.read(Path(file))
     }
     jsonFile = readFile(Path('<your/notion/config/file/path>'))
-    var notonInfo = JSON.parse(jsonFile)
-    let functionChoices = Object.keys(notonInfo["pages"]).concat(["Custom"])
+    var notionInfo = JSON.parse(jsonFile)
+    let functionChoices = Object.keys(notionInfo["pages"]).concat(["Custom"])
 
     // Fetch user selection 
     var pageSelection = app.chooseFromList(functionChoices, {
         withPrompt: "Which page do you want to add to?",
         defaultItems: functionChoices[0]
     })
-
+	
     // Parse the user selection
     if (pageSelection[0] == "Custom") {
         // Allow user input
@@ -28,8 +34,10 @@ function run(input, parameters) {
             defaultButton: "Continue"
         })
         returnVals.push(response.textReturned)
+		var pageUrl=response.textReturned;
     } else if (pageSelection[0] != undefined) {
         returnVals.push(pageSelection[0])
+		var pageUrl=notionInfo["pages"][pageSelection[0]];
     } else (
         returnVals.push(false)
     )
@@ -37,14 +45,15 @@ function run(input, parameters) {
     // Fetch the data from the given application
     titleVals = ["--titles"]
     urlVals = ["--urls"]
-    var Safari = Application('Safari');
-    Safari.includeStandardAdditions = true;
-    Safari.windows[0].tabs().forEach(function (tab) {
-        // console.log(tab.url())
+    Browser.windows[0].tabs().forEach(function (tab) {
         urlVals.push(tab.url())
         titleVals.push(tab.name())
     })
 
+    // Open the notion page
+	var notionSchema = pageUrl.replace("https://", "notion://")
+	app.doShellScript(`open "${notionSchema}"`)
+    
     // Return the values 
     returnVals = returnVals.concat(titleVals).concat(urlVals)
     return returnVals
